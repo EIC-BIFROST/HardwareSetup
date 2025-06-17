9457fb5a168294108fdb1d77a1189aef610b5acb
bxnchichi <benchi2507@gmail.com>
2025-06-17 00:48:19 +0000

    armed status for hardware test

---
+++ RCconnect/armCheck.py
@@ -0,0 +1,22 @@
+from mavsdk import System
+import asyncio
+
+async def check_arm_status():
+    drone = System()
+    await drone.connect(system_address="udp://:14540")
+
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"-- Connected to drone!")
+            break
+    while True:
+        async for armed in drone.telemetry.armed():
+            print(f"Is armed: {armed}")
+            break
+        if armed:
+            break
+
+
+if __name__ == "__main__":
+    asyncio.run(check_arm_status())
