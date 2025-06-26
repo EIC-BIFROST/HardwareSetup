09c669f08140b7d57be94a3774dcd9bfe1dd438f
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2025-06-26 17:23:09 +0700

    Update start.sh

--- QuickRun/start.sh
+++ QuickRun/start.sh
@@ -5,12 +5,12 @@ tmux new-session -d -s drone_session
 # Window 1: MAVLink router
 tmux rename-window -t drone_session 'MAVLink'
 tmux send-keys -t drone_session \
-  'mavlink-routerd -e 127.0.0.1:14550 -e 127.0.0.1:14551 -e 127.0.0.1:14552 /dev/serial/by-id/usb-Auterion_PX4_v6X.x_0-if00-port0:57600' C-m
+  'mavlink-routerd -e 127.0.0.1:14551 -e 127.0.0.1:14552 /dev/serial/by-path/platform-3610000.usb-usb-0\:2.3\:1.0:57600' C-m
 
 # Window 2: Docker Python execution
 tmux split-window -h -t drone_session
 tmux send-keys -t drone_session \
-  'docker start eic && docker exec -it eic bash -c "cd /workspace/home/jetson/Autonomous_Drone && python3 run.py"' C-m
+  'docker start eic && docker exec -it eic bash -c "cd /workspace/home/jetson/Autonomous_Drone && python3 run.py; exec bash"' C-m
 
 # Optional: layout
 tmux select-layout -t drone_session tiled
